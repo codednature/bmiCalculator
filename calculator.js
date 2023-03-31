45c9ea3c5e25acdb2a0175e1123f2669a475f35e
naturesola <100572919+naturesola@users.noreply.github.com>
2023-03-31 01:16:46 +0100

    initial commit

---
+++ calculator.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+app.get("/bmicalculator", function(req, res){
+    res.sendFile(__dirname+"/bmiCalculator.html");
+});
+
+app.get("/bmicalculator", function(req, res){
+    res.sendFile(__dirname+"/bmiCalculator.html");
+});
+
+app.post("/bmicalculator", function(req, res){
+
+    var Weight = parseFloat(req.body.weight);
+    var Height = parseFloat(req.body.height);
+
+    var  bmi = Weight / (Height * Height);
+
+    console.log(Weight);
+
+    res.send("Your BMI is " +  bmi);
+});
+
+
+app.get("/", function(req, res){
+        res.sendFile(__dirname + "/index.html");
+    });
+    
+    app.post("/", function(req, res){
+    
+        var num1 = Number(req.body.num1);
+        var num2 = Number(req.body.num2);
+    
+        var result = num1 + num2;
+    
+        res.send("The result of the calculation is " +  result)
+    });
+    
+
+
+app.listen(3000, function() {
+    console.log("Calculator app listening on port 3000")
+});
