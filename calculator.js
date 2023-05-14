31816d6fc0c6cac0dc7e398e037da573661df257
naturesola <100572919+naturesola@users.noreply.github.com>
2023-05-14 16:37:37 +0100

    edits

--- calculator.js
+++ calculator.js
@@ -9,18 +9,13 @@ app.get("/bmicalculator", function(req, res){
     res.sendFile(__dirname+"/bmiCalculator.html");
 });
 
-app.get("/bmicalculator", function(req, res){
-    res.sendFile(__dirname+"/bmiCalculator.html");
-});
 
 app.post("/bmicalculator", function(req, res){
 
-    var Weight = parseFloat(req.body.weight);
-    var Height = parseFloat(req.body.height);
+    const Weight = parseFloat(req.body.weighted);
+    const Height = parseFloat(req.body.height);
 
-    var  bmi = Weight / (Height * Height);
-
-    console.log(Weight);
+    const  bmi = Weight / (Height * Height);
 
     res.send("Your BMI is " +  bmi);
 });
@@ -32,10 +27,10 @@ app.get("/", function(req, res){
     
     app.post("/", function(req, res){
     
-        var num1 = Number(req.body.num1);
-        var num2 = Number(req.body.num2);
+        const num1 = Number(req.body.num1);
+        const num2 = Number(req.body.num2);
     
-        var result = num1 + num2;
+        const result = num1 + num2;
     
         res.send("The result of the calculation is " +  result)
     });
